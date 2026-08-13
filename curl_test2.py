3a66289a851dcf054ef986183693932da07d5d9e
Cyber Brain <cyberbrain24@gmail.com>
2026-08-13 21:57:55 +0600

    Create curl_test2.py

---
+++ curl_test2.py
@@ -0,0 +1,8 @@
+import paramiko
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('185.227.134.236', username='root', password='PoshPlex')
+stdin, stdout, stderr = ssh.exec_command("curl -I --insecure https://127.0.0.1 -H 'Host: store.poshplexbd.com'")
+print("STDOUT:", stdout.read().decode('utf-8', errors='ignore'))
+print("STDERR:", stderr.read().decode('utf-8', errors='ignore'))
+ssh.close()
