228aa94c8db02e4bfe66737f1a054d660fb6ab78
Cyber Brain <cyberbrain24@gmail.com>
2026-08-20 04:04:59 +0600

    ..

--- poshplex_store/next.config.js
+++ poshplex_store/next.config.js
@@ -39,6 +39,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'media.poshplexbd.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'api.poshplexbd.com',
         pathname: '/media/**',
       },
