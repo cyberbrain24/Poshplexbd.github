3986b7cc28b7f647bdd610f423157ab546bca167
Cyber Brain <cyberbrain24@gmail.com>
2026-08-20 04:33:22 +0600

    ..

--- poshplex/urls.py
+++ poshplex/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 from poshplex.api import api
 
 urlpatterns = [
@@ -12,3 +14,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # Force Django to serve media in production (Cloudflare will cache it)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
